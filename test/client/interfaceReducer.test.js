6ee32cba6f7d273a3f9d67a0c77a8b323e370c76
changsailong <changsailong>
2026-09-12 02:59:37 +0800

    test: 增加接口详情失败回归测试

--- test/client/interfaceReducer.test.js
+++ test/client/interfaceReducer.test.js
@@ -3,7 +3,7 @@ import reducer from '../../client/reducer/modules/interface';
 
 const FETCH_INTERFACE_DATA = 'yapi/interface/FETCH_INTERFACE_DATA';
 
-const payload = id => ({data: {data: {_id: id, title: '接口 ' + id}}});
+const payload = id => ({data: {errcode: 0, data: {_id: id, title: '接口 ' + id}}});
 
 test('接口详情 reducer 忽略先发后至的旧响应', t => {
   const initial = reducer(undefined, {});
@@ -20,4 +20,21 @@ test('接口详情 reducer 忽略先发后至的旧响应', t => {
 
   t.is(current.curdata._id, 2);
   t.is(stale.curdata._id, 2);
+});
+
+
+test('接口详情请求失败时保留已有详情', t => {
+  const initial = reducer(undefined, {});
+  const current = reducer(initial, {
+    type: FETCH_INTERFACE_DATA,
+    requestId: 1,
+    payload: payload(1)
+  });
+  const failed = reducer(current, {
+    type: FETCH_INTERFACE_DATA,
+    requestId: 2,
+    payload: {data: {errcode: 400, errmsg: '接口不存在', data: null}}
+  });
+
+  t.is(failed.curdata._id, 1);
 });
