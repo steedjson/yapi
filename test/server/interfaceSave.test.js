964bfe0bac5cfd5252c22a01f2651ae647b993f5
changsailong <changsailong>
2026-09-12 08:27:18 +0800

    test: 增加导入接口编辑保存回归

--- test/server/interfaceSave.test.js
+++ test/server/interfaceSave.test.js
@@ -1,6 +1,7 @@
 import test from 'ava';
 
 const controller = require('rewire')('../../server/controllers/interface');
+const InterfaceController = controller.__get__('interfaceController');
 const handleHeaders = controller.__get__('handleHeaders');
 
 // 导入数据可能缺少请求头或表单字段，保存前应补齐可用的默认请求头。
@@ -54,4 +55,41 @@ test('导入数据缺少表单数组时仍可补齐请求头', t => {
   t.deepEqual(values.req_headers, [
     { name: 'Content-Type', value: 'application/x-www-form-urlencoded' }
   ]);
+});
+
+
+test('导入接口编辑保存成功后返回成功结果', async t => {
+  controller.__set__('yapi', {
+    commons: {
+      verifyPath: () => true,
+      resReturn: (data, errcode, message) => ({ data, errcode: errcode || 0, message }),
+      validateParams: () => ({ valid: true })
+    }
+  });
+  const instance = Object.create(InterfaceController.prototype);
+  let updatedParams;
+  instance.$tokenAuth = true;
+  instance.schemaMap = { up: {} };
+  instance.Model = {
+    getByPath: async () => [{ _id: 17, res_body: '{}', path: '/users', method: 'GET' }]
+  };
+  instance.up = async ctx => {
+    updatedParams = ctx.params;
+    ctx.body = { errcode: 0 };
+  };
+
+  const ctx = {
+    params: {
+      project_id: 1,
+      path: '/users',
+      method: 'get',
+      title: '导入用户接口'
+    }
+  };
+
+  await instance.save(ctx);
+
+  t.is(updatedParams.id, 17);
+  t.is(updatedParams.method, 'GET');
+  t.is(ctx.body.errcode, 0);
 });
