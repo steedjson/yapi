85e2211359e1711ee206d15d536f63e780b2e9b6
changsailong <changsailong>
2026-09-13 01:03:55 +0800

    refactor: 增加 createContext 与 defaultTheme 类型检查

--- common/createContext.js
+++ common/createContext.js
@@ -1,3 +1,11 @@
+// @ts-check
+/**
+ * 将用户、项目、接口标识转换为数值上下文
+ * @param {string|number} uid
+ * @param {string|number} projectId
+ * @param {string|number} interfaceId
+ * @returns {{ uid: number, projectId: number, interfaceId: number }}
+ */
 module.exports = function (uid, projectId,interfaceId) {
   if(!uid || !projectId || !interfaceId){
     console.error('uid projectId interfaceId 不能为空', uid, projectId,interfaceId)
--- server/utils/reportHtml/defaultTheme.js
+++ server/utils/reportHtml/defaultTheme.js
@@ -1,4 +1,10 @@
-const fs = require('fs');
-const sysPath = require('path');
+// @ts-check
+/**
+ * @param {string} name
+ * @returns {any}
+ */
+const requireAny = name => require(name);
+const fs = requireAny('fs');
+const sysPath = requireAny('path');
 const css = fs.readFileSync(sysPath.join(__dirname, './defaultTheme.css'));
 module.exports = '<style>' + css + '</style>';
