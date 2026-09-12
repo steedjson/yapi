54b879308433bf0dbe856d3c7d953c202b7cf211
changsailong <changsailong>
2026-09-12 09:16:01 +0800

    fix: 兼容生产资源清单历史键名

--- build/webpack.standalone.config.js
+++ build/webpack.standalone.config.js
@@ -94,7 +94,15 @@ module.exports = {
       new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false } }),
       new AssetsPlugin({
         filename: 'static/prd/assets.js',
-        processOutput: assets => 'window.WEBPACK_ASSETS = ' + JSON.stringify(assets)
+        processOutput: assets => {
+          // 保持 static/index.html 依赖的历史 index.js manifest key。
+          const normalized = Object.assign({}, assets);
+          if (normalized.index && !normalized['index.js']) {
+            normalized['index.js'] = normalized.index;
+            delete normalized.index;
+          }
+          return 'window.WEBPACK_ASSETS = ' + JSON.stringify(normalized);
+        }
       }),
       new CompressionPlugin({
         asset: '[path].gz[query]',
